636d7473ec3e8bf3730089ffb380bf1696ce5ebd
deepakdj007 <deepakjose000@gmail.com>
2024-01-28 12:19:03 +0530

    making info section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './common/nav-bar/nav-bar.component';
-
+import { NavBarComponent } from './components/common/nav-bar/nav-bar.component';
+import { BioComponent } from './components/bio/bio.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, BioComponent],
 })
 export class AppComponent {
   title = 'portfolio_frontend';
