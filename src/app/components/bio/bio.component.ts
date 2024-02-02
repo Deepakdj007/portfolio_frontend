ca51f289474148fb6cdd4bfaa8c004ea458f302a
deepakdj007 <deepakjose000@gmail.com>
2024-02-02 13:09:20 +0530

    added image & animations

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { PrimaryButtonComponent } from '../common/primary-button/primary-button.component';
 @Component({
   selector: 'app-bio',
   standalone: true,
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './bio.component.html',
-  styleUrl: './bio.component.scss'
+  styleUrl: './bio.component.scss',
 })
-export class BioComponent {
-
-}
+export class BioComponent {}
--- src/app/components/common/primary-button/primary-button.component.ts
+++ src/app/components/common/primary-button/primary-button.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   standalone: true,
   imports: [],
   templateUrl: './primary-button.component.html',
-  styleUrl: './primary-button.component.scss'
+  styleUrl: './primary-button.component.scss',
 })
 export class PrimaryButtonComponent {
-
+  @Input() buttonName!: string;
 }
